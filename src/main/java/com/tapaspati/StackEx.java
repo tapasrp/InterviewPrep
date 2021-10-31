35e2b4a38713c5985e9f4fc3612431357b3bb483
tapasrp <tapasranjanpati@live.com>
2021-10-30 22:32:32 -0400

    :)

--- src/main/java/com/tapaspati/StackEx.java
+++ src/main/java/com/tapaspati/StackEx.java
@@ -1,5 +1,18 @@
 package com.tapaspati;
+
+import java.util.Stack;
+
 public class StackEx {
+
+    public static void main(String[] args) {
+        //create a stack , push and pop elements
+
+
+        Stack<Integer> intStack = new Stack<>();
+
+        intStack.push(1);
+        System.out.println(intStack.toString());
+     }
 
 
 }
